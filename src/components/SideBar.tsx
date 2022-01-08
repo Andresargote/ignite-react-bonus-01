caff7c0cf7129d3fa48adf745a5b6a7193044f34
Andres Argote <andresargote@pickerexpress.com>
2022-01-08 16:40:01 +0100

    All working!

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,36 @@
-export function SideBar() {
+import { Button } from './Button';
+
+import '../styles/sidebar.scss';
+
+interface genreProps {
+  id: number;
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  title: string;
+}
+interface SideBarProps {
+  genres: genreProps[];
+  selectedGenreId: number;
+  handleClickButton: (id: number) => void;
+}
+
+export function SideBar(props: SideBarProps) {
   // Complete aqui
+  return (
+    <nav className="sidebar">
+      <span>Watch<p>Me</p></span>
+
+      <div className="buttons-container">
+        {props.genres.map(genre => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            selected={props.selectedGenreId === genre.id}
+            onClick={() => props.handleClickButton(genre.id)}
+          />
+        ))}
+      </div>
+
+    </nav>
+  );
 }
